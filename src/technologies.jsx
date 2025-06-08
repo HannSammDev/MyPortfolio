2fe80c33b8b7b9834bb284831f64128c6f6a3820
Hann Samm A. Beleganio <beleganiohannsamm@gmail.com>
2025-06-08 23:06:50 +0800

    fewchange

--- src/technologies.jsx
+++ src/technologies.jsx
@@ -28,73 +28,73 @@ export const Technologies = () => {
                                 data-aos='fade-up'
                                 alt="HTML5"
                                 src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg"
-                                className="w-10 h-10 border border-transparent hover:border-orange-500 hover:shadow-lg hover:shadow-orange-500/50 transform hover:scale-110 transition-transform duration-200"
+                                className="w-10 h-10 border border-transparent hover:border-orange-500 hover:shadow-lg hover:shadow-orange-500/50 transform hover:scale-110 transition-transform duration-300"
                             />
                             <img
                                 data-aos='fade-up'
                                 alt="CSS3"
                                 src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg"
-                                className="w-10 h-10 border border-transparent hover:border-blue-500 hover:shadow-lg hover:shadow-blue-500/50 transform hover:scale-110 transition-transform duration-200"
+                                className="w-10 h-10 border border-transparent hover:border-blue-500 hover:shadow-lg hover:shadow-blue-500/50 transform hover:scale-110 transition-transform duration-300"
                             />
                             {/* <img
                             data-aos='fade-up'
                                 alt="Tailwind CSS"
                                 src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/tailwindcss/tailwindcss-plain.svg"
-                                className="w-10 h-10 border border-transparent hover:border-blue-500 hover:shadow-lg hover:shadow-blue-500/50 transform hover:scale-110 transition-transform duration-200"
+                                className="w-10 h-10 border border-transparent hover:border-blue-500 hover:shadow-lg hover:shadow-blue-500/50 transform hover:scale-110 transition-transform duration-300"
                             /> */}
                             <img
                                 data-aos='fade-up'
                                 alt="Bootstrap"
                                 src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/bootstrap/bootstrap-original.svg"
-                                className="w-10 h-10 border border-transparent hover:border-violet-500 hover:shadow-lg hover:shadow-violet-500/50 transform hover:scale-110 transition-transform duration-200"
+                                className="w-10 h-10 border border-transparent hover:border-violet-500 hover:shadow-lg hover:shadow-violet-500/50 transform hover:scale-110 transition-transform duration-300"
                             />
                             <img
                                 data-aos='fade-up'
                                 alt="JavaScript"
                                 src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg"
-                                className="w-10 h-10 border border-transparent hover:border-yellow-500 hover:shadow-lg hover:shadow-yellow-500/50 transform hover:scale-110 transition-transform duration-200"
+                                className="w-10 h-10 border border-transparent hover:border-yellow-500 hover:shadow-lg hover:shadow-yellow-500/50 transform hover:scale-110 transition-transform duration-300"
                             />
                             <img
                                 data-aos='fade-up'
                                 alt="React"
                                 src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg"
-                                className="w-10 h-10 border border-transparent hover:border-blue-300 hover:shadow-lg hover:shadow-blue-500/50 transform hover:scale-110 transition-transform duration-200"
+                                className="w-10 h-10 border border-transparent hover:border-blue-300 hover:shadow-lg hover:shadow-blue-500/50 transform hover:scale-110 transition-transform duration-300"
                             />
                             <img
                                 data-aos='fade-up'
                                 alt="Firebase"
                                 src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/firebase/firebase-plain.svg"
-                                className="w-10 h-10 border border-transparent hover:border-yellow-500 hover:shadow-lg hover:shadow-yellow-500/50 transform hover:scale-110 transition-transform duration-200"
+                                className="w-10 h-10 border border-transparent hover:border-yellow-500 hover:shadow-lg hover:shadow-yellow-500/50 transform hover:scale-110 transition-transform duration-300"
                             />
                             <img
                                 data-aos='fade-up'
                                 alt="Node.js"
                                 src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original.svg"
-                                className="w-10 h-10 border border-transparent hover:border-green-500 hover:shadow-lg hover:shadow-green-500/50 transform hover:scale-110 transition-transform duration-200"
+                                className="w-10 h-10 border border-transparent hover:border-green-500 hover:shadow-lg hover:shadow-green-500/50 transform hover:scale-110 transition-transform duration-300"
                             />
                             <img
                                 data-aos='fade-up'
                                 alt="PHP"
                                 src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/php/php-original.svg"
-                                className="w-10 h-10 border border-transparent hover:border-violet-500 hover:shadow-lg hover:shadow-violet-500/50 transform hover:scale-110 transition-transform duration-200"
+                                className="w-10 h-10 border border-transparent hover:border-violet-500 hover:shadow-lg hover:shadow-violet-500/50 transform hover:scale-110 transition-transform duration-300"
                             />
                             <img
                                 data-aos='fade-up'
                                 alt="GitHub"
                                 src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg"
-                                className="w-10 h-10 border border-transparent hover:border-gray-950 hover:shadow-lg hover:shadow-dark transform hover:scale-110 transition-transform duration-200"
+                                className="w-10 h-10 border border-transparent hover:border-gray-950 hover:shadow-lg hover:shadow-dark transform hover:scale-110 transition-transform duration-300"
                             />
                             <img
                                 data-aos='fade-up'
                                 alt="Python"
                                 src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/python/python-original.svg"
-                                className="w-10 h-10 border border-transparent hover:border-blue-500 hover:shadow-lg hover:shadow-blue-500/50 transform hover:scale-110 transition-transform duration-200"
+                                className="w-10 h-10 border border-transparent hover:border-blue-500 hover:shadow-lg hover:shadow-blue-500/50 transform hover:scale-110 transition-transform duration-300"
                             />
                             <img
                                 data-aos='fade-up'
                                 alt="Git"
                                 src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-original.svg"
-                                className="w-10 h-10 border border-transparent hover:border-orange-500 hover:shadow-lg hover:shadow-orange-500/50 transform hover:scale-110 transition-transform duration-200"
+                                className="w-10 h-10 border border-transparent hover:border-orange-500 hover:shadow-lg hover:shadow-orange-500/50 transform hover:scale-110 transition-transform duration-300"
                             />
 
                         </div>
